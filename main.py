7da4897203eb700f61614064532a4f1d533f4bd9
Jeff Waters <watersjg@gmail.com>
2024-04-30 19:39:36 +0100

    Deleted classes applying normalisation and standardisation, except to Support Vector Machine, as using normalisation and standardisation with those algorithms made little or no difference.
    Updated README results screenshot in light of this and provided updated  results.csv file.

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from machine_learning_algorithm.linear_regression_no_standardisation_or_normalisation import \
     LinearRegressionNoStandardisationOrNormalisation
-from machine_learning_algorithm.linear_regression_with_normalisation import LinearRegressionWithNormalization
-from machine_learning_algorithm.linear_regresssion_with_standardisation import LinearRegressionWithStandardization
+
 from machine_learning_algorithm.polynomial_regression_no_standardisation_or_normalisation import \
     PolynomialRegressionNoStandardisationOrNormalisation
-from machine_learning_algorithm.polynomial_regression_with_normalisation import PolynomialRegressionWithNormalization
-from machine_learning_algorithm.polynomial_regression_with_standardisation import \
-    PolynomialRegressionWithStandardization
+
 from machine_learning_algorithm.random_forest_no_standardisation_or_normalisation import \
     RandomForestRegressionNoStandardisationOrNormalisation
-from machine_learning_algorithm.random_forest_with_normalisation import RandomForestRegressionWithNormalization
-from machine_learning_algorithm.random_forest_with_standardisation import RandomForestRegressionWithStandardization
+
 from machine_learning_algorithm.support_vector_machine_no_standardisation_or_normalisation import \
     SVMRegressionNoStandardisationOrNormalisation
+
 from machine_learning_algorithm.support_vector_machine_with_standardisation import SVMRegressionWithStandardization
 from machine_learning_algorithm.support_vector_with_machine_normalisation import SVMRegressionWithNormalization
 
@@ -20,16 +17,10 @@ from machine_learning_algorithm.support_vector_with_machine_normalisation import
 def perform_and_publish_tests() -> None:
     algorithms = [
         LinearRegressionNoStandardisationOrNormalisation(),
-        LinearRegressionWithNormalization(),
-        LinearRegressionWithStandardization(),
 
         PolynomialRegressionNoStandardisationOrNormalisation(),
-        PolynomialRegressionWithNormalization(),
-        PolynomialRegressionWithStandardization(),
 
         RandomForestRegressionNoStandardisationOrNormalisation(),
-        RandomForestRegressionWithNormalization(),
-        RandomForestRegressionWithStandardization(),
 
         SVMRegressionNoStandardisationOrNormalisation(),
         SVMRegressionWithNormalization(),
